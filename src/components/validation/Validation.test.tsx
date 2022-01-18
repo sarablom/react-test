8bb161c6d5d02685e5c14c79e6c0fa93783e755b
sarablom <sara.cecilia.blom@gmail.com>
2022-01-18 16:41:58 +0100

    add tests for component validation

--- src/components/validation/Validation.test.tsx
+++ src/components/validation/Validation.test.tsx
@@ -11,4 +11,31 @@ describe("validation component", () => {
         expect(inputField).toBeEmptyDOMElement();
         expect(inputField).not.toHaveClass('valid');
     })
+
+    it("css class 'valid' added when user writes valid name", () => {
+        render(<Validation />);
+
+        const inputField = screen.getByRole('textbox');
+        userEvent.type(inputField, 'Sara');
+
+        expect(inputField).toHaveClass('valid');
+    })
+
+    it("css class 'invalid' added when user writes invalid name", () => {
+        render(<Validation />);
+
+        const inputField = screen.getByRole('textbox');
+        userEvent.type(inputField, 'Sara32');
+
+        expect(inputField).toHaveClass('invalid');
+    })
+
+    it("show message 'Var vänlig skriv ditt namn' when input field is empty", () => {
+        render(<Validation />);
+        
+        const message = screen.getByText(/Var vänlig skriv ditt namn/i);
+        
+
+        expect(message).toBeInTheDocument();
+    })
 })
--- src/components/validation/Validation.tsx
+++ src/components/validation/Validation.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 
 function Validation() {
+
+    const [isValid, setIsValid] = useState(false);
+    const [message, setMessage] = useState(true);
+
+    function onChangeHandler (e: any) {
+        setMessage(false);
+        if (/^[a-zA-Z\s]*$/.test(e.target.value)) {  
+            setIsValid(true);
+        } else {
+            setIsValid(false); 
+            setMessage(true);           
+        }       
+    }
+
     return (
         <form>
-            <input type="text" />
+            <input type="text" onChange={(e) => onChangeHandler(e)} className={isValid ? "valid" : "invalid"} />
+            {message ? <p>"Var vänlig skriv ditt namn"</p> : null}
         </form>
     )
 }
